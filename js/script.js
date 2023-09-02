d8d9a67c8b92bf91bec859e156bb49ae49e8973d
jcolago <jcolago2005@gmail.com>
2023-09-02 17:27:33 -0500

    Added delete employee function to js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,15 @@ let totalSpent = 0;
 $(onReady);
 //This will run listed functions when the DOM is ready
 function onReady() {
-    $('#submit').on('click', submitInfo)
+
+    //This will run the submitInfo function when submit button is clicked.
+    $('#submit').on('click', submitInfo);
+
+    //This will delete an employee from the table if the delete button is clicked.
+    $('#deleteButton').on('click', deleteEmployee);
+
+    //This listend on the info id for a click event on the delete button id. This will then run deleteEmployee function.
+    $('#info').on('click', '#deleteButton', deleteEmployee);
 }
 
 
@@ -25,12 +33,12 @@ let monthlySpent = annualSalary/12;
 
     //Appends the HTML to add a table row with the entered input data and displays it on the DOM. Also adds a delete button to the row.
     $('#info').append(
-        `<tr>
+        `<tr id="employee">
         <td>${firstName}</td>
         <td>${lastName}</td>
         <td>${idNumber}</td>
         <td>${title}</td>
-        <td>${annualSalary}</td>
+        <td id="currentSalary">$ ${annualSalary}</td>
         <td><button id="deleteButton">Delete</button></td>
         </tr>`)
         $('#deleteButton').css('text-align', 'center');
@@ -43,14 +51,18 @@ let monthlySpent = annualSalary/12;
 
     //This takes the monthlySpent and adds it to the total spent. This also displays the total spending for the month on the DOM
      totalSpent += Number(monthlySpent);
-  $('#monthlyTotal').text(Math.round(totalSpent));
-
+  $('#monthlyTotal').text(totalSpent.toFixed(2));
+//If totalSpent is over $20,000, the background will turn red.
   if(totalSpent >= 20000){
     $('#monthlyTotal').css('background-color', 'red');
   }
 
 }
 
+//This will delete the row of employee info when delete is clicked.
+function deleteEmployee(event){
+ $(event.target).closest('#employee').remove();
+}
 
 
 
